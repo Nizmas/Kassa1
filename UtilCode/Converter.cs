d703caa25f324faec0bdb7be467f43423be3bb3a
Nizmas <64789614+Nizmas@users.noreply.github.com>
2020-07-14 22:23:49 +1000

    Relize

---
+++ UtilCode/Converter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Office.Interop.Word;
+
+namespace Kassa1.UtilCode
+{
+    public class Converter
+    {
+        public string ConvertFile(string pathDoc)
+        {
+
+            string parhPdf = pathDoc.Replace(".docx", ".pdf");
+
+            Application appWord = new Application();
+            var wordDocument = appWord.Documents.Open(pathDoc);
+            wordDocument.ExportAsFixedFormat(parhPdf, WdExportFormat.wdExportFormatPDF);
+
+            return parhPdf;
+        }
+    }
+}
